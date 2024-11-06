02892972881b801aa21efc664a862b10c38b76f9
Igor Motov <igor@motovs.org>
2024-11-06 07:05:00 -1000

    Update IntelliJ setup

--- oh-my-zsh/custom/00_macos.zsh
+++ oh-my-zsh/custom/00_macos.zsh
@@ -44,8 +44,12 @@ if [[ $OS == "Darwin" ]]; then
     fi
 
     if /usr/libexec/java_home 1>/dev/null 2>&1; then
-        export JAVA_HOME=`/usr/libexec/java_home -v17`
+        export JAVA_HOME=`/usr/libexec/java_home -v21`
         export ES_JAVA_HOME="$JAVA_HOME"
+    fi
+
+    if [ -d "/Applications/IntelliJ IDEA CE.app/Contents/MacOS" ]; then
+        export PATH=$PATH:"/Applications/IntelliJ IDEA CE.app/Contents/MacOS"
     fi
 
 fi
